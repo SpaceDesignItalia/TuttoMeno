be2156ca764bfc84674c437973ee461a39d4a7b4
Andrea Braia <andrix.braia@gmail.com>
2024-02-11 19:15:09 +0100

    New Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,7 @@
-'use client'
-
 import Image from 'next/image'
-import logoImage from '@/images/assicurazioni/qrwechat.png'
+import logoImage from '@/images/assicurazioni/QRWeChat.png'
 
-export function Logo() {
+export function Logo(props) {
   return (
     <div>
       <Image src={logoImage} alt="logo" className=" h-40 w-40 rounded-lg" />
