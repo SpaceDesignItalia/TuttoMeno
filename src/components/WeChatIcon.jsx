2f842380a36f9901b468d2d51135af00768cec7c
Andrea Braia <andrix.braia@gmail.com>
2024-02-11 19:44:23 +0100

    Icona WeChat

--- src/components/WeChatIcon.jsx
+++ src/components/WeChatIcon.jsx
@@ -4,7 +4,11 @@ import logoImage from '@/images/assicurazioni/WeChaticon.png'
 export function WeChatIcon(props) {
   return (
     <div className="flex flex-row items-center gap-2 text-white">
-      <Image src={logoImage} alt="logo" className=" h-8 w-8" />
+      <Image
+        src="https://tuttomeno.com/media/WeChaticon.7345e847.png"
+        alt="logo"
+        className=" h-8 w-8"
+      />
       WeChat
     </div>
   )
