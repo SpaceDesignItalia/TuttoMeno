eb80e03907d85aadd5af777b6e29560f928d1feb
Andrea Braia <andrix.braia@gmail.com>
2024-02-11 19:12:55 +0100

    Logo weChat

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
-import logoImage from '@/images/assicurazioni/QRWeChat.png'
+'use client'
 
-export function Logo(props) {
+import Image from 'next/image'
+import logoImage from '@/images/assicurazioni/qrwechat.png'
+
+export function Logo() {
   return (
     <div>
       <Image src={logoImage} alt="logo" className=" h-40 w-40 rounded-lg" />
