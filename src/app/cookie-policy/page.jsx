31b8e5f003a23e1aee5b9fa797e9152092517523
Andrea Braia <andrix.braia@gmail.com>
2024-05-18 16:16:53 +0200

    Fix 1

--- src/app/cookie-policy/page.jsx
+++ src/app/cookie-policy/page.jsx
@@ -1,9 +1,3 @@
-import Link from 'next/link'
-
-import { Button } from '@/components/Button'
-import { SelectField, TextField } from '@/components/Fields'
-import { Logo } from '@/components/Logo'
-import { SlimLayout } from '@/components/SlimLayout'
 import { Header } from './Header'
 import { Footer } from './Footer'
 import CookieConsent from '../CookieConsent'
