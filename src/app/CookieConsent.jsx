230ed27405aa4c1e4b948988947eb2c50235db15
Andrea Braia <andrix.braia@gmail.com>
2024-05-18 16:26:42 +0200

    Phone fix

--- src/app/CookieConsent.jsx
+++ src/app/CookieConsent.jsx
@@ -27,7 +27,7 @@ export default function CookieConsent() {
   }
 
   return (
-    <div className="fixed bottom-0 flex w-full items-center justify-between bg-gray-800 p-4 text-white">
+    <div className="fixed bottom-0 flex w-full flex-col items-center justify-between gap-5 bg-gray-800 p-4 text-white sm:flex-row">
       <p className="m-0">
         Questo sito utilizza i cookie per migliorare l&apos;esperienza
         dell&apos;utente.{' '}
@@ -35,7 +35,7 @@ export default function CookieConsent() {
           Leggi di più
         </a>
       </p>
-      <div>
+      <div className="flex flex-col gap-5 sm:flex-row">
         <button
           onClick={handleAccept}
           className="mr-2 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
